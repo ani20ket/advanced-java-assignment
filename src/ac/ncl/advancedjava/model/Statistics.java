2512c05346d6469be6dae4e7846f78fe792224a7
Aniket Thorat <ani20ket@Anikets-MacBook-Air.local>
2024-10-01 00:14:46 +0100

    added student and statistics class TBC

--- src/ac/ncl/advancedjava/model/Statistics.java
+++ src/ac/ncl/advancedjava/model/Statistics.java
@@ -1,4 +1,20 @@
 package ac.ncl.advancedjava.model;
 
-public class Statistics {
+import java.time.LocalDate;
+
+public class Statistics extends Student {
+    private FinalVerdict finalVerdict;
+    private int noOfAttempts;
+    private int noOfRevisions;
+
+
+    public Statistics(String name, int age, LocalDate birthDate) {
+        super(name, age, birthDate);
+    }
+
+
+
+    public enum FinalVerdict{
+        PASS, FAIL, TBD;
+    }
 }
--- src/ac/ncl/advancedjava/model/Student.java
+++ src/ac/ncl/advancedjava/model/Student.java
@@ -1,4 +1,52 @@
 package ac.ncl.advancedjava.model;
 
+import java.time.LocalDate;
+
 public class Student {
+    private String name;
+    private int age;
+    private LocalDate birthDate;
+    private int id;
+
+    public Student(String name, int age, LocalDate birthDate) {
+        this.name = name;
+        this.age = age;
+        this.birthDate = birthDate;
+    }
+
+    public String toString() {
+        return "id: " + id + ", name: " + name + ", age: " + age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
